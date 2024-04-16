efcdd1b22d90ae01ad4b02a4b8f05af9ca1c9404
Davi Araújo <daviaraujo3078@gmail.com>
2024-04-16 11:04:20 -0300

    feat(ap05): fim da secao 2.7
    - Define o estado do componente principal
    - Define a funcao que obtem estacao climatica
    - Define nome de icone por estacao climatica

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -5,6 +5,46 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 //componente definido via classe 
 class App extends React.ComponenT{
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            estacao: null,
+            data: null,
+            icone: null
+        }
+    }
+
+    obterEstacao = (data, latitude) => {
+        const anoAtual = data.getFullYear()
+        // 21/06
+        const d1 = new Date(anoAtual, 5, 21)
+        // 24/09
+        const d2 = new Date(anoAtual, 8, 24)
+        // 22/12
+        const d3 = new Date(anoAtual, 11, 22)
+        // 21/03
+        const d4 = new Date(anoAtual, 2, 21)
+
+        // operador ternário
+        const estouNoSul = latitude < 0 ? true : false
+        if (data >= d1 && data < d2)
+            return estouNoSul ? 'Inverno' : 'Verão'
+        if (data >= d2 && data < d3)
+            return estouNoSul ? 'Primavera' : 'Outono'
+        if (data >= d3 && data < d4)
+            return estouNoSul ? 'Verão' : 'Inverno'
+        return estouNoSul ? 'Outono' : 'Primavera'
+    }
+
+    icones = {
+        'Primavera': 'fa-seedling',
+        'Verão': 'fa-sun',
+        'Outono': 'fa-canadian-maple-leaf',
+        'Inverno': 'fa-snowflake'
+    }
+
     render(){
         return(
             <div>Meu app</div>
